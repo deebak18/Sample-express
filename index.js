ca6a02533527aac88c74c84c784f95769dce6bdf
deebak18 <deebakperumal@gmail.com>
2023-01-22 18:12:46 +0530

    add new route

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const app = express()
 app.get("/", (_, res) =>{
     res.send('server up')
 })
+
 app.get("/home", (_, res) =>{
     res.send('home up')
 })
+
 app.get("/about", (_, res) =>{
     res.send('about up')
+})
+
+app.get("/contact", (_, res) =>{
+    res.send('contact up')
 })
 
 app.listen(process.env.PORT||5000, () =>{
